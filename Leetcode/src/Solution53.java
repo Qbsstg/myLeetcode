c7f38bab8c221e46c5da7750de00b8d0384c2611
Qbsstg <45097135+Qbsstg@users.noreply.github.com>
2020-10-09 00:48:30 +0800

    10.9

--- Leetcode/src/Solution53.java
+++ Leetcode/src/Solution53.java
@@ -1,3 +1,5 @@
+import org.jetbrains.annotations.NotNull;
+
 public class Solution53 {
 
     /*
@@ -16,9 +18,34 @@ public class Solution53 {
     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
 
-    public int maxSubArray(int[] nums) {
-        return 0;
+    public int maxSubArray(int @NotNull [] nums) {
+
+        int max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            int temp = nums[i];
+            if (temp <= 0) {
+                max = Math.max(max, temp);
+            } else {
+                max = Math.max(max, temp);
+                i++;
+                for (; i < nums.length; i++) {
+                    temp += nums[i];
+                    if (temp > 0) {
+                        max = Math.max(max, temp);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return max;
     }
 
+
+    public static void main(String[] args) {
+        Solution53 Solution53 = new Solution53();
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(Solution53.maxSubArray(nums));
+    }
 
 }
