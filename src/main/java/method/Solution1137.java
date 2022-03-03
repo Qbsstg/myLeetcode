25925fbee5dc29488790e70661178efb3d24e24a
Qbsstg <wuhao5207@gmail.com>
2022-03-03 11:20:14 +0800

    1137

---
+++ src/main/java/method/Solution1137.java
@@ -0,0 +1,62 @@
+package method;
+
+/**
+ * @author Qbss
+ * @date 2022/3/3
+ * @desc
+ */
+public class Solution1137 {
+
+    public static void main(String[] args) {
+        System.out.println(tribonacci(4));
+        System.out.println(tri(4));
+        System.out.println(t(4));
+    }
+
+    public static int tribonacci(int n) {
+
+        if (n == 0 || n == 1) {
+            return n;
+        } else if (n == 2) {
+            return 1;
+        } else {
+            return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+        }
+    }
+
+    public static int tri(int n) {
+
+        int[] a = new int[n + 1];
+
+        a[0] = 0;
+        a[1] = 1;
+        a[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            a[i] = a[i - 1] + a[i - 2] + a[i - 3];
+        }
+
+        return a[n];
+    }
+
+    public static int t(int n) {
+
+        int a = 0;
+        int b = 1;
+        int c = 1;
+
+        int d = 0;
+
+        for (int i = 3; i <= n; i++) {
+            d = a + b + c;
+
+            a = b;
+            b = c;
+            c = d;
+
+        }
+        return d;
+    }
+
+
+}
--- src/main/java/method/Solution509.java
+++ src/main/java/method/Solution509.java
@@ -8,8 +8,10 @@ package method;
 public class Solution509 {
 
     public static void main(String[] args) {
-        System.out.println(fib(5));
-        System.out.println(fb(6));
+        //System.out.println(fib(5));
+        //System.out.println(fb(5));
+        System.out.println(f(5));
+
     }
 
 
@@ -17,28 +19,44 @@ public class Solution509 {
 
         if (n == 0 || n == 1) {
             return n;
-        }
-        if (n >= 2) {
+        } else {
             return fib(n - 1) + fib(n - 2);
         }
-        return 0;
     }
 
     public static int fb(int n) {
 
+
+        //暂存的概念
+        int[] a = new int[n + 1];
+
+        a[0] = 0;
+        a[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            a[i] = a[i - 1] + a[i - 2];
+        }
+        return a[n];
+    }
+
+    public static int f(int n) {
+
         if (n == 0 || n == 1) {
             return n;
         }
+
         int a = 0;
         int b = 1;
-        int c = 0;
+        int c = a + b;
 
-        for (int i = 2; i < n + 1; i++) {
+        for (int i = 2; i <= n; i++) {
             c = a + b;
             a = b;
             b = c;
         }
+
         return c;
     }
+
 
 }
