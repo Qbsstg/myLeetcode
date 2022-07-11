b95111d15c5c1e515e52944b721e451cc38757a6
Qbsstg <wuhao5207@gmail.com>
2022-07-11 14:34:24 +0800

    278题题解提交

---
+++ src/main/java/method/Solution278.java
@@ -0,0 +1,20 @@
+package method;
+
+/**
+ * @author Qbss
+ * @date 2022/7/11
+ * @desc
+ */
+public class Solution278 {
+
+    //1,2,3(false),4(true),5(true)
+    public int firstBadVersion(int n) {
+
+    }
+
+
+    static boolean isBadVersion(int version) {
+        return version > 100;
+    }
+
+}
