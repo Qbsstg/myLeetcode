63968b08dc86e8fa6f9052eeb28f029d7c3db42f
Qbsstg <wuhao5207@gmail.com>
2022-07-20 18:30:29 +0800

    542题

--- src/main/java/method/Solution542.java
+++ src/main/java/method/Solution542.java
@@ -1,6 +1,8 @@
 package method;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
 
 /**
  * @author Qbss
@@ -10,8 +12,26 @@ import java.util.*;
 public class Solution542 {
 
     public static void main(String[] args) {
-        int[][] mat = {{0, 0, 0}, {0, 1, 0}, {1, 1, 1}};
+        //int[][] mat = {{0, 0, 0}, {0, 1, 0}, {1, 1, 1}};
+        //int[][] mat = {{0, 1, 0}, {0, 1, 0}, {0, 1, 0}, {0, 1, 0}, {0, 1, 0}};
+
+        int[][] mat =
+                {
+                        {1, 1, 0, 0, 1, 0, 0, 1, 1, 0},
+                        {1, 0, 0, 1, 0, 1, 1, 1, 1, 1},
+                        {1, 1, 1, 0, 0, 1, 1, 1, 1, 0},
+                        {0, 1, 1, 1, 0, 1, 1, 1, 1, 1},
+                        {0, 0, 1, 1, 1, 1, 1, 1, 1, 0},
+                        {1, 1, 1, 1, 1, 1, 0, 1, 1, 1},
+                        {0, 1, 1, 1, 1, 1, 1, 0, 0, 1},
+                        {1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+                        {0, 1, 0, 1, 1, 0, 1, 1, 1, 1},
+                        {1, 1, 1, 0, 1, 0, 1, 1, 1, 1}
+                };
+
+
         System.out.println(Arrays.deepToString(updateMatrix(mat)));
+        //System.out.println(mat1[1][1]);
     }
 
 
@@ -24,44 +44,59 @@ public class Solution542 {
 
         int n = mat.length;
         int m = mat[0].length;
-        int ans[][] = new int[n][m];
+        int[][] ans = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ans[i][j] = -1;
+            }
+        }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 int value = mat[i][j];
                 if (value == 1) {
-                    List<Integer> count = new ArrayList<>();
-                    Queue<int[]> queue = new ArrayDeque<>();
-                    queue.offer(new int[]{i, j});
-                    while (!queue.isEmpty()) {
-                        int[] poll = queue.poll();
-                        Queue<int[]> ints = new ArrayDeque<>();
-                        for (int k = 0; k < 4; k++) {
-                            int mx = poll[0] + dx[k];
-                            int my = poll[1] + dy[k];
-                            if (mx >= 0 && my >= 0 && mx < n && my < m) {
-                                int info = mat[mx][my];
-                                ints.offer(new int[]{mx, my});
-                                if (info == 0) {
-                                    count.add(mx);
-                                    count.add(my);
-                                    ints.clear();
-                                    queue.clear();
-                                    break;
+                    if (ans[i][j] == -1) {
+                        int count = 0;
+                        Queue<int[]> queue = new ArrayDeque<>();
+                        queue.offer(new int[]{i, j});
+                        while (!queue.isEmpty()) {
+                            count++;
+                            int size = queue.size();
+                            for (int s = 0; s < size; s++) {
+                                int[] poll = queue.poll();
+                                int temp = -1;
+                                for (int k = 0; k < 4; k++) {
+                                    int mx = poll[0] + dx[k];
+                                    int my = poll[1] + dy[k];
+                                    if (mx >= 0 && my >= 0 && mx < n && my < m) {
+                                        if (mat[mx][my] == 0) {
+                                            queue.clear();
+                                            s = size;
+                                            break;
+                                        } else {
+                                            queue.offer(new int[]{mx, my});
+                                            if (ans[mx][my] != -1) {
+                                                temp = temp == -1 ? ans[mx][my] : Math.min(temp,
+                                                        ans[mx][my]);
+                                            }
+                                        }
+                                        if (k == 3) {
+                                            if (temp != -1) {
+                                                count = count + temp;
+                                                queue.clear();
+                                                s = size;
+                                                break;
+                                            }
+                                        }
+                                    }
                                 }
                             }
                         }
-                        queue.addAll(ints);
-                    }
-                    if (count.isEmpty()) {
-                        ans[i][j] = -1;
-                    } else {
-                        ans[i][j] = Math.abs(count.get(0) - i) + Math.abs(count.get(1) - j);
+                        ans[i][j] = count;
                     }
                 } else {
                     ans[i][j] = 0;
                 }
-                mat[i][j] = -1;
             }
         }
         return ans;
