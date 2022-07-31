0adbeb01f65d6afc81c2070944b993103c085c05
Qbsstg <wuhao5207@gmail.com>
2022-07-31 22:20:16 +0800

    6134

--- src/main/java/method/Solution6134.java
+++ src/main/java/method/Solution6134.java
@@ -1,9 +1,61 @@
 package method;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Solution6134 {
 
-    public int closestMeetingNode(int[] edges, int node1, int node2) {
-        return 0;
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 3, -1};
+        int[] nums2 = {1, 2, -1};
+        int[] nums1 = {5, -1, 3, 4, 5, 6, -1, -1, 4, 3};
+        int[] nums3 = {4, 4, 4, 5, 1, 2, 2};
+        int[] nums4 = {9, 8, 7, 0, 5, 6, 1, 3, 2, 2};
+        int[] nums5 = {5, 3, 1, 0, 2, 4, 5};
+
+
+        //System.out.println(closestMeetingNode(nums, 0, 1));
+        //System.out.println(closestMeetingNode(nums2, 0, 2));
+        //System.out.println(closestMeetingNode(nums1, 0, 0));
+        //System.out.println(closestMeetingNode(nums3, 1, 1));
+        //System.out.println(closestMeetingNode(nums2, 0, 2));
+        //System.out.println(closestMeetingNode(nums4, 1, 6));
+        System.out.println(closestMeetingNode(nums5, 3, 2));
+    }
+
+    public static int closestMeetingNode(int[] edges, int node1, int node2) {
+
+        if (node1 == node2) {
+            return node1;
+        }
+
+        int length = edges.length;
+        boolean[] used = new boolean[length];
+
+        int node = node1;
+        used[node] = true;
+        do {
+            node = edges[node];
+            if (used[node]) {
+                break;
+            }
+            used[node] = true;
+        } while (edges[node] != -1);
+
+        if (used[node2] && edges[node2] == -1) {
+            return node2;
+        }
+
+        int targetNode = node2;
+        while (edges[targetNode] != -1) {
+            if (used[targetNode]) {
+                return targetNode;
+            }
+            targetNode = edges[targetNode];
+        }
+        return -1;
     }
 
 }
