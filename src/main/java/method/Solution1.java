02a6b0784e38556e3b4e527a0fab5bc1d8678677
Qbsstg <wuhao5207@gmail.com>
2022-08-02 11:08:51 +0800

    数据结构学习第二日

--- src/main/java/method/Solution1.java
+++ src/main/java/method/Solution1.java
@@ -1,6 +1,6 @@
 package method;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Solution1 {
 
@@ -9,9 +9,9 @@ public class Solution1 {
         int[] reNums = new int[2];
         for (int i = 0; i < nums.length; i++) {
             int a = nums[i];
-            for (int j = i+1; j < nums.length; j++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 int b = nums[j];
-                if (a+b == target){
+                if (a + b == target) {
                     reNums[0] = i;
                     reNums[1] = j;
                     break;
@@ -21,17 +21,42 @@ public class Solution1 {
         return reNums;
     }
 
+    public static int[] twoSum1(int[] nums, int target) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            List<Integer> orDefault = map.getOrDefault(nums[i], new ArrayList<>());
+            orDefault.add(i);
+            map.put(nums[i], orDefault);
+        }
+
+        for (Map.Entry<Integer, List<Integer>> next : map.entrySet()) {
+            Integer key = next.getKey();
+            int i = target - key;
+
+            if (i == key) {
+                List<Integer> value = next.getValue();
+                if (value.size() > 1) {
+                    return new int[]{value.get(0), value.get(1)};
+                }
+            } else {
+                List<Integer> list = map.get(i);
+                if (list != null) {
+                    return new int[]{next.getValue().get(0), list.get(0)};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
 
     public static void main(String[] args) {
-        int[] nums = {3,3};
+        int[] nums = {3, 3};
         int target = 6;
-        int[] reNums = twoSum(nums,target);
-
+        int[] reNums = twoSum1(nums, target);
 
 
         System.out.println(Arrays.toString(reNums));
     }
-
 
 
 }
---
+++ src/main/java/method/Solution88.java
@@ -0,0 +1,58 @@
+package method;
+
+/**
+ * @author Qbss
+ * @date 2022/8/2
+ * @desc
+ */
+public class Solution88 {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+        merge(nums1, m, nums2, n);
+    }
+
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int[] res = new int[m + n];
+        int[] maxNums;
+        int[] minNums;
+
+        if (m >= n) {
+            maxNums = nums1;
+            minNums = nums2;
+        } else {
+            maxNums = nums2;
+            minNums = nums1;
+            int temp = n;
+            n = m;
+            m = temp;
+        }
+
+        for (int i = 0, j = 0, k = 0; i < m && k < m + n; i++, k++) {
+            int i1 = maxNums[i];
+            for (; j < n; j++, k++) {
+                if (i1 >= minNums[j]) {
+                    res[k] = minNums[j];
+                } else {
+                    break;
+                }
+            }
+            res[k] = i1;
+            if (i == m - 1) {
+                k = k + 1;
+                for (; j < n; j++, k++) {
+                    res[k] = minNums[j];
+                }
+            }
+        }
+
+        System.arraycopy(res, 0, nums1, 0, m + n);
+    }
+
+
+}
