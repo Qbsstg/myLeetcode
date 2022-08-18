dba510f180e0e2b1e0af9272e31a4aa89b4caabe
Qbsstg <wuhao5207@gmail.com>
2022-08-18 23:42:54 +0800

    重写list方法

--- src/main/java/test/QbssList.java
+++ src/main/java/test/QbssList.java
@@ -55,6 +55,81 @@ public class QbssList<E> extends AbstractList<E> {
         }
     }
 
+    private Object[] grow(int minCapacity) {
+        int oldCapacity = elementData.length;
+        if (oldCapacity > 0 || elementData != DEFAULTCAPACITY_EMPTY_ELEMENTDATA) {
+
+            int newCapacity;
+
+            int preLength = oldCapacity + Math.max(minCapacity - oldCapacity, oldCapacity >> 1);
+            if (preLength > 0 && preLength <= Integer.MAX_VALUE - 8) {
+                newCapacity = preLength;
+            } else {
+                int minLength = oldCapacity + (minCapacity - oldCapacity);
+                if (minLength < 0) {
+                    throw new OutOfMemoryError("数组的长度太大");
+                } else if (minLength <= Integer.MAX_VALUE - 8) {
+                    newCapacity = Integer.MAX_VALUE - 8;
+                } else {
+                    newCapacity = minCapacity;
+                }
+            }
+            return elementData = Arrays.copyOf(elementData, newCapacity);
+        }
+        //数组原长度为0的情况下，直接按照指定的大小与默认10的最大值返回一个新数组
+        else {
+            return elementData = new Object[Math.max(DEFAULT_CAPACITY, minCapacity)];
+        }
+    }
+
+    private Object[] grow() {
+        //数组想要扩容，输入想要的最小扩容大小
+        return grow(size + 1);
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    //在数组中指定开始后结束位置查找第一次出现该元素的下标
+    int indexOfRange(Object o, int start, int end) {
+
+        Object[] es = elementData;
+
+        if (start < 0 || end >= size) {
+            return -1;
+        }
+        if (o == null) {
+            for (int i = start; i < end; i++) {
+                if (es[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = start; i < end; i++) {
+                if (es[i].equals(o)) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    /*
+     * 在list中添加一位，但其实对于数组来说可能还没到最后一个位置
+     * 因为数组的长度是动态扩容后的，所以elementData.length的值是大于等于list.size()的
+     * 所以判断，当list的长度即数组中实际保存元素的长度 与 数组的长度相等时，list开始扩容
+     * */
+    public boolean add(E e) {
+
+        if (size == elementData.length) {
+            elementData = grow();
+        }
+        elementData[size] = e;
+        size += 1;
+        return true;
+    }
 
     @Override
     public E get(int index) {
--- src/main/java/test/test5.java
+++ src/main/java/test/test5.java
@@ -1,7 +1,5 @@
 package test;
 
-import common.Node;
-
 /**
  * @author Qbss
  * @date 2022/8/12
@@ -9,14 +7,44 @@ import common.Node;
  */
 public class test5 {
 
+
+    public static final int SOFT_MAX_ARRAY_LENGTH = Integer.MAX_VALUE - 8;
+
+    public static int newLength(int oldLength, int minGrowth, int prefGrowth) {
+        // preconditions not checked because of inlining
+        // assert oldLength >= 0
+        // assert minGrowth > 0
+
+        int prefLength = oldLength + Math.max(minGrowth, prefGrowth); // might overflow
+        if (0 < prefLength && prefLength <= SOFT_MAX_ARRAY_LENGTH) {
+            return prefLength;
+        } else {
+            // put code cold in a separate method
+            return hugeLength(oldLength, minGrowth);
+        }
+    }
+
+    private static int hugeLength(int oldLength, int minGrowth) {
+        int minLength = oldLength + minGrowth;
+        if (minLength < 0) { // overflow
+            throw new OutOfMemoryError(
+                    "Required array length " + oldLength + " + " + minGrowth + " is too large");
+        } else if (minLength <= SOFT_MAX_ARRAY_LENGTH) {
+            return SOFT_MAX_ARRAY_LENGTH;
+        } else {
+            return minLength;
+        }
+    }
+
     public static void main(String[] args) {
 
-        Node node = new Node(1);
-        Node node1 = new Node(2);
+        System.out.println(newLength(2, 1, 2 >> 1));
+
+        int a = Integer.MAX_VALUE;
+        System.out.println(a+1);
 
 
     }
-
 
 
 }
