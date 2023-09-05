c7b90c606902d81108947b94e200e7a7f3b52537
wuhao <wuhao5207@gmail.com>
2023-09-05 10:19:52 +0800

    2605. 从两个数字数组里生成最小数字

---
+++ src/main/java/method/Solution2605.java
@@ -0,0 +1,18 @@
+package method;
+
+public class Solution2605 {
+
+
+    public int minNumber(int[] nums1, int[] nums2) {
+
+        int result = Integer.MAX_VALUE;
+
+        for (int a : nums1) {
+            for (int b : nums2) {
+                int value = a == b ? a : a > b ? b * 10 + a : a * 10 + b;
+                result = Math.min(result, value);
+            }
+        }
+        return result;
+    }
+}
