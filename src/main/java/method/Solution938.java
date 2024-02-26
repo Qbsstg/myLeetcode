5eb0794f7420a0811488b5cd5977f0e08400ddf8
Qbss <wuhao5207@gmail.com>
2024-02-26 11:30:02 +0800

    938. 二叉搜索树的范围和

---
+++ src/main/java/method/Solution938.java
@@ -0,0 +1,33 @@
+package method;
+
+import common.Node;
+
+/**
+ * @author: Qbss
+ * @date: 2024/2/26
+ * @desc: 938. 二叉搜索树的范围和
+ */
+public class Solution938 {
+
+    public int rangeSumBST(Node root, int low, int high) {
+
+        int result = 0;
+
+        return result + dfs(root, low, high);
+
+    }
+
+    private int dfs(Node node, int low, int high) {
+        if (node == null) {
+            return 0;
+        }
+
+        if (node.val >= low && node.val <= high) {
+            return node.val + dfs(node.left, low, high) + dfs(node.right, low, high);
+        }
+
+        return dfs(node.left, low, high) + dfs(node.right, low, high);
+
+    }
+
+}
